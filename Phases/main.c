f602169e1caaa144a071dc9dd85c57cc44f4ac6d
rmartinez96 <raul.martinez01@utrgv.edu>
2017-10-22 17:57:31 -0500

    Initial Commit

---
+++ Phases/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Phases
+//
+//  Created by Raul Martinez on 10/22/17.
+//  Copyright © 2017 Raul Martinez. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
